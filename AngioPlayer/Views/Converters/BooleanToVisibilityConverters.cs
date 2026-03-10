c7114f221b51db14ae33f59fe3c4b84a7bfe689a
alexvorona <alexvorona@yahoo.com>
2026-03-10 09:36:48 +0200

    Add Play/Pause button visibility toggle using converters in XAML

---
+++ AngioPlayer/Views/Converters/BooleanToVisibilityConverters.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace AngioPlayer.Views.Converters;
+
+public class BooleanToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+        => (value is bool b && b) ? Visibility.Visible : Visibility.Collapsed;
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+        => (value is Visibility v && v == Visibility.Visible);
+}
+
+public class InverseBooleanToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+        => (value is bool b && b) ? Visibility.Collapsed : Visibility.Visible;
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+        => (value is Visibility v && v == Visibility.Visible) == false;
+}
